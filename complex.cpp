08bf36e219eb8d9a6867e9f056c5d2102b5b5663
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-03-07 20:20:16 +0530

    refactor complex class to use operator overloading for addition, subtraction, and unary negation

--- complex.cpp
+++ complex.cpp
@@ -7,7 +7,9 @@ class complex
     public:
         void setdata(int x,int y);
         void showdata();
-        complex add(complex c2);
+        complex operator +(complex c2);
+        complex operator -(complex c2);
+        complex operator -();
 };
 void complex::setdata(int x,int y)
     {
@@ -18,20 +20,38 @@ void complex::showdata()
     {
         cout<<endl<<"a = "<<a<<" b = "<<b;
     }
-complex complex::add(complex c2)
+complex complex::operator +(complex c2)
 {
     complex temp;
     temp.a=a+c2.a;
     temp.b=b+c2.b;
     return temp;
 }
+complex complex::operator -(complex c)
+{
+    complex temp;
+    temp.a=a-c.a;
+    temp.b=b-c.b;
+    return temp;
+}
+complex complex::operator -()
+{
+    complex temp;
+    temp.a=-a;
+    temp.b=-b;
+    return temp;
+}
 int main()
 {
-    complex c1,c2,c3;
+    complex c1,c2,c3,c4,c5;
     c1.setdata(3,4);
     c2.setdata(5,6);
     c1.showdata();
     c2.showdata();
-    c3=c1.add(c2);
+    c3=c1+c2;    //c3=c1.operator +(c2);
+    c4=c1-c2;    //c4=c1.operator -(c2);
     c3.showdata();
+    c4.showdata();
+    c5=-c1;      //c5=c1.operator -();
+    c5.showdata();
 }
